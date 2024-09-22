3ba55efcaac99cadc37b633d46b9f981d5171b08
Paloma Nascimento <paloma13744@gmail.com>
2024-09-22 17:20:30 -0300

    Configuração front-end

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./navbar.scss"
 import logo from './yopflix.png'
+import { ArrowDropDown, Notifications, Search } from '@material-ui/icons'
 
-function Navbar() {
+const Navbar = () =>  {
+  const[isScrolled , setIsScrolled] = useState(false); // Scrool da tela  (topo , estado inicial)
+
+  window.onscroll = () =>{   // função para rolagem do scrool
+
+  }
+  
   return (
     <div className='navbar'>
         <div className='container'>
             <div className='esquerda'>
                 <img src= {logo} alt ="Logo da Yopflix"  ></img>
             </div>
-            <span>Página Inicial</span>
+            <span>Home</span>
             <span>Séries</span>
             <span>Filmes</span>
             <span>Filmes Populares</span>
             <span>Minha lista</span>
 
             <div className='direita'>
+              <Search className='icon'></Search>
+              <span>KID</span>
+              <Notifications className='icon'></Notifications> 
+              {/* Imagem de perfil */}
+              <img src='https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt=''></img>
+
+              <div className="profile">
+               <ArrowDropDown className='icon'></ArrowDropDown>
+               <div className="options">      {/* Mini menu de opçoes para o usuário*/}
+                <span>Configurações</span>
+                <span>Sair</span>
+
+               </div>
+              </div>
+
+            
 
             </div>
         </div>
